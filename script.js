d4f6bd1711333459567731a82e37a162fe07eef3
Jose Neil Silagan <neilsilagan@gmail.com>
2024-06-28 11:52:14 +0800

    Add the function constructor, addBookToLibrarr, and Array myLibrary
    Also add the initialBook that we will be working on

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(Book){
+    myLibrary.push(Book);
+}
+
+const initialBook = new Book("Meditation", "Marcus Aurelius", 200, false)
+addBookToLibrary(initialBook);
