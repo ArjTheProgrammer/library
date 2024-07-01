fcbfe2726262bce6c915d0ab2a23394992bfd5cc
Jose Neil Silagan <neilsilagan@gmail.com>
2024-07-01 16:32:25 +0800

    Display input when clicked confirm

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-function addBookToLibrary(Book){
-    myLibrary.push(Book);
+function addBookToLibrary(title, author, pages, read){
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
-const initialBook = new Book("Meditation", "Marcus Aurelius", 200, true);
-addBookToLibrary(initialBook);
 
-const initialBook2 = new Book("Subtle art of not giving a fuck", "Mark Manson", 200, false);    
-addBookToLibrary(initialBook2);
-console.log(myLibrary);
+addBookToLibrary("Meditation", "Marcus Aurelius", 200, true);
+addBookToLibrary("Subtle art of not giving a fuck", "Mark Manson", 200, false);
 
-function DisplayBooks(){
+myLibrary.forEach(book => {
+    DisplayBooks(book);
+});
+
+function DisplayBooks(book){
     const container = document.querySelector(".container");
-
-  myLibrary.forEach((book, index) => {
     const card = document.createElement("div");
     card.className = `card`;
 
@@ -41,7 +41,6 @@ function DisplayBooks(){
     card.append(read);
 
     container.appendChild(card);
-}); 
 }
 
 /*button listener*/
@@ -49,12 +48,23 @@ const dialog = document.querySelector("dialog");
 const openDialog = document.querySelector("dialog + .addBook");
 const closeDialog = document.querySelector("dialog .closeDialog");
 
+const confirmButton = document.querySelector("#confirm");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+
 openDialog.addEventListener("click", () => {
     dialog.showModal();
 });
 
-closeDialog.addEventListener("click", () => {
+closeDialog.addEventListener("click", (e) => {
+    e.preventDefault();
     dialog.close();
 });
 
-DisplayBooks();
+confirmButton.addEventListener("click", () => {
+    console.log(`${title.value} ${author.value} ${pages.value} ${read.checked ? true:false}`); 
+    addBookToLibrary(title.value, author.value, pages.value, read.checked ? true:false);
+    DisplayBooks(myLibrary[myLibrary.length - 1]);
+})
